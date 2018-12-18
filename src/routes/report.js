00e1bd68039044612cd356f875e6fa93c36b8027
OKORO OBINNA <obinna.okoro@tenece.com>
2018-12-18 09:47:27 +0100

    added new route for message

--- src/routes/report.js
+++ src/routes/report.js
@@ -5,7 +5,7 @@ const _router = require('express').Router(),
 
 _router.get('/', _reportController.findAll.bind(_reportController));
 _router.post('/photo', _base64ToFile("uploads[]"), _reportController.save.bind(_reportController));
-_router.post('/message', _base64ToFile("uploads[]"), _reportController.save.bind(_reportController));
+_router.post('/message', _reportController.save.bind(_reportController));
 _router.get('/:id', _reportController.findById.bind(_reportController));
 _router.put('/', _reportController.update.bind(_reportController));
 _router.delete('/:id', _reportController.delete.bind(_reportController));
